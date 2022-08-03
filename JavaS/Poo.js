c343df20ee16d753c86bff4bd9fcb75fa94a15ed
guillerfacu <guillerfacu@gmail.com>
2022-08-02 23:01:35 -0300

    Vimos poo tipo json con java, y utilizando tambien function

---
+++ JavaS/Poo.js
@@ -0,0 +1,37 @@
+const persona1 ={
+    nombre : 'Homero',
+    edad :  39,
+    calle : 'Av, SiempreViva 742'
+
+}
+
+/* tipo json */
+/* console.log(persona1.nombre);
+console.log(persona1.edad);
+console.log(persona1.calle); */
+ 
+/* otra clase */
+
+const persona2 ={
+    nombre : 'Bart',
+    edad :  10,
+    calle : 'Av, SiempreViva 742'
+
+}
+
+/*------------------ Constructor------------- */
+
+function Persona(nombre, edad, calle){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.calle = calle;
+    this.hablar = function(){ console.log("Hola Soy "+ this.nombre)}
+}
+const persona3 = new Persona("Homero", 39, "Av. SiempreViva 742");
+const persona4 = new Persona("Bart", 10, "Av. SiempreViva 742");
+
+
+console.log(persona3.nombre);
+console.log(persona3.edad);
+console.log(persona3.calle);
+persona3.hablar();
